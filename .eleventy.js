4b8930524e265259f9e34e2caf8924c3a1f42e56
Erich Grunewald <erichgrunewald@gmail.com>
2022-09-15 22:22:19 +0200

    Add stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,15 @@
 module.exports = function (eleventyConfig) {
-    return {
-        templateFormats: [
-            "md",
-            "njk",
-            "html",
-            "png",
-            "jpg",
-        ],
-        passthroughFileCopy: true,
-        markdownTemplateEngine: "liquid",
-        htmlTemplateEngine: "njk",
-        dataTemplateEngine: "njk",
-        pathPrefix: "/mount-fog-website"
-    };
+  // copy images and css to site dir
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("fonts");
+
+  return {
+    templateFormats: ["md", "njk", "html", "png", "jpg"],
+    passthroughFileCopy: true,
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    pathPrefix: "/mount-fog-website",
+  };
 };
